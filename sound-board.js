a0936fef1db9ab71d3663287da7bbc79b4e77b0e
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-30 18:34:44 -0500

    added more sounds

--- sound-board.js
+++ sound-board.js
@@ -1,24 +1,45 @@
 // GLOBAL VARS
-const villagers = document.getElementsByClassName("btnVillager");
+const villagers = document.getElementsByClassName("villager");
 const zombieWalker = document.getElementsByClassName("zombieWalker");
 const tromboneWaWaWa = document.getElementsByClassName("trumbone-sad");
-
-
+const yodaDied = document.getElementsByClassName("yoda")
+const bruh = document.getElementsByClassName("bruh")
+const babyatripple = document.getElementsByClassName("babyatripple")
+const farts = document.getElementsByClassName("fart")
 
 // GLOBAL FUNCTIONS
 const villagerSound = () => {
     let sound = new Audio('audio/villager.mp3');
     sound.play();
 };
+
 const zombieWalkerSound = () => {
     let sound = new Audio('audio/zombie-walker.mp3');
     sound.play();
 };
+
 const tromboneWaWaWaSound = () => {
     let sound = new Audio('audio/Trombone.mp3');
     sound.play();
 };
 
+const yodaSound = () => {
+    let sound = new Audio('audio/yoda.mp3');
+    sound.play();
+};
+
+const bruhSound = () => {
+    let sound = new Audio('audio/bruh.mp3');
+    sound.play();
+};
+const babyatrippleSound = () => {
+    let sound = new Audio('audio/babyatripple.mp3');
+    sound.play();
+};
+const fartSound = () => {
+    let sound = new Audio('audio/fart.mp3');
+    sound.play();
+};
 // GLOBAL EVENTS
 
 for (const villager of villagers) {
@@ -31,14 +52,43 @@ for (const villager of villagers) {
 for (const zombie of zombieWalker) {
     zombie.addEventListener("click", () => {
         console.log("FIRED");
-        // Call the villagerSound function if needed
+        // Call the zombie function if needed
         zombieWalkerSound()
     });
 }
 for (const wawa of tromboneWaWaWa) {
     wawa.addEventListener("click", () => {
         console.log("FIRED");
-        // Call the villagerSound function if needed
+        // Call the trombone function if needed
         tromboneWaWaWaSound()
     });
 }
+for (const yoda of yodaDied) {
+    yoda.addEventListener("click", () => {
+        console.log("FIRED");
+        // Call the yodaSound
+        yodaSound()
+    });
+}
+for (const bro of bruh) {
+    bro.addEventListener("click", () => {
+        console.log("FIRED");
+        // Call the bruhSound function
+        bruhSound()
+    });
+}
+for (const oh of babyatripple) {
+    oh.addEventListener("click", () => {
+        console.log("FIRED");
+        // Call the babyatrippleSound function
+        babyatrippleSound()
+    });
+}
+for (const fart of farts) {
+    fart.addEventListener("click", () => {
+        console.log("FIRED");
+        // Call the fartSound function
+        fartSound()
+    });
+}
+
